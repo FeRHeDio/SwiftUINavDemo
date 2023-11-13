6dd608dc5bf8282f1ef3bc80dfac1c802042061f
Fernando Putallaz <ferhedio@gmail.com>
2023-11-13 18:13:21 -0300

    Project sarted successfully

---
+++ SwiftUINavDemo/SwiftUINavDemo/SwiftUINavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavDemoApp.swift
+//  SwiftUINavDemo
+//
+//  Created by Fernando Putallaz on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
