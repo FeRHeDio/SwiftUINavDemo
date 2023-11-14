351b4ad959f27c50fa090d8c35c2fe1d42750ae7
Fernando Putallaz <ferhedio@gmail.com>
2023-11-14 13:36:52 -0300

    Initial Dog Model within a List view.

--- SwiftUINavDemo/SwiftUINavDemo/ContentView.swift
+++ SwiftUINavDemo/SwiftUINavDemo/ContentView.swift
@@ -8,17 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  let dogsVM = DogViewModel()
+  
+  var body: some View {
+    List(dogsVM.dogs) { dog in
+      Text(dog.name)
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
---
+++ SwiftUINavDemo/SwiftUINavDemo/Dog.swift
@@ -0,0 +1,49 @@
+//
+//  Dog.swift
+//  SwiftUINavDemo
+//
+//  Created by Fernando Putallaz on 13/11/2023.
+//
+
+import Foundation
+
+struct Dog: Identifiable {
+  var id = UUID()
+  var name: String
+}
+
+struct DogViewModel {
+  var dogs = [
+      Dog(name: "Max"),
+      Dog(name: "Buddy"),
+      Dog(name: "Charlie"),
+      Dog(name: "Rocky"),
+      Dog(name: "Duke"),
+      Dog(name: "Bear"),
+      Dog(name: "Tucker"),
+      Dog(name: "Jack"),
+      Dog(name: "Toby"),
+      Dog(name: "Cooper"),
+      Dog(name: "Hunter"),
+      Dog(name: "Sam"),
+      Dog(name: "Milo"),
+      Dog(name: "Buster"),
+      Dog(name: "Bailey"),
+      Dog(name: "Scout"),
+      Dog(name: "Abby"),
+      Dog(name: "Maggie"),
+      Dog(name: "Leo"),
+      Dog(name: "Lucy"),
+      Dog(name: "Lola"),
+      Dog(name: "Daisy"),
+      Dog(name: "Molly"),
+      Dog(name: "Sadie"),
+      Dog(name: "Chloe"),
+      Dog(name: "Sophie"),
+      Dog(name: "Bella"),
+      Dog(name: "Luna"),
+      Dog(name: "Gracie"),
+      Dog(name: "Ellie"),
+      Dog(name: "Zoey")
+    ]
+}
