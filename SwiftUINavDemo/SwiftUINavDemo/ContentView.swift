d29a0f2a9db2b96f169aeb99052ea896e7acc6db
Fernando Putallaz <ferhedio@gmail.com>
2023-11-18 09:39:37 -0300

    Adds Cat model + Screens enum for navigation

--- SwiftUINavDemo/SwiftUINavDemo/ContentView.swift
+++ SwiftUINavDemo/SwiftUINavDemo/ContentView.swift
@@ -7,18 +7,42 @@
 
 import SwiftUI
 
+enum Screens: Hashable {
+  case dog(Dog)
+  case cat(Cat)
+}
+
 struct ContentView: View {
-  @State var presentedDogs: [Dog] = []
+  @State var path: [Screens] = []
+  
   let dogVM = DogViewModel()
+  let catsVM = CatsViewModel()
 
   var body: some View {
-    NavigationStack(path: $presentedDogs) {
-      List(dogVM.dogs) { dog in
-        NavigationLink(dog.name, value: dog)
-       }
-      .navigationTitle("Dogs World")
-      .navigationDestination(for: Dog.self) { dog in
-        DogDetails(dog: dog)
+    NavigationStack(path: $path) {
+      List {
+        Section("Dogs") {
+          ForEach(dogVM.dogs.prefix(5)) { dog in
+            NavigationLink(dog.name, value: Screens.dog(dog))
+          }
+        }
+        
+        
+        Section("Cats") {
+          ForEach(catsVM.cats.prefix(5)) { cat in
+            NavigationLink(cat.name, value: Screens.cat(cat))
+          }
+          
+        }
+      }
+      .navigationTitle("World of pets")
+      .navigationDestination(for: Screens.self) { screen in
+        switch screen {
+        case .dog(let dog):
+          DogDetails(dog: dog)
+        case .cat(let cat):
+          CatDetails(cat: cat)
+        }
       }
     }
   }
@@ -34,6 +58,14 @@ struct DogDetails: View {
   
   var body: some View {
     Text(dog.name)
+  }
+}
+
+struct CatDetails: View {
+  let cat: Cat
+  
+  var body: some View {
+    Text(cat.name)
   }
 }
 
--- SwiftUINavDemo/SwiftUINavDemo/Dog.swift
+++ SwiftUINavDemo/SwiftUINavDemo/Dog.swift
@@ -12,6 +12,11 @@ struct Dog: Identifiable, Hashable {
   var name: String
 }
 
+struct Cat: Identifiable, Hashable {
+  var id = UUID()
+  var name: String
+}
+
 struct DogViewModel {
   var dogs = [
       Dog(name: "Max"),
@@ -45,5 +50,40 @@ struct DogViewModel {
       Dog(name: "Gracie"),
       Dog(name: "Ellie"),
       Dog(name: "Zoey")
+    ]
+}
+
+struct CatsViewModel {
+  var cats = [
+    Cat(name: "Whiskers"),
+    Cat(name: "Mittens"),
+    Cat(name: "Shadow"),
+    Cat(name: "Luna"),
+    Cat(name: "Oliver"),
+    Cat(name: "Cleo"),
+    Cat(name: "Simba"),
+    Cat(name: "Mocha"),
+    Cat(name: "Bella"),
+    Cat(name: "Tiger"),
+    Cat(name: "Misty"),
+    Cat(name: "Leo"),
+    Cat(name: "Salem"),
+    Cat(name: "Nala"),
+    Cat(name: "Oreo"),
+    Cat(name: "Charlie"),
+    Cat(name: "Ziggy"),
+    Cat(name: "Coco"),
+    Cat(name: "Max"),
+    Cat(name: "Lucy"),
+    Cat(name: "Mittens"),
+    Cat(name: "Shadow"),
+    Cat(name: "Luna"),
+    Cat(name: "Oliver"),
+    Cat(name: "Cleo"),
+    Cat(name: "Simba"),
+    Cat(name: "Mocha"),
+    Cat(name: "Bella"),
+    Cat(name: "Tiger"),
+    Cat(name: "Misty")
     ]
 }
