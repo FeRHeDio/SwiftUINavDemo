fecd80677b167e55ca3ca255b3791810597c2822
Fernando Putallaz <ferhedio@gmail.com>
2023-11-22 09:38:31 -0300

    Give Cat model its own file.

---
+++ SwiftUINavDemo/SwiftUINavDemo/Cat.swift
@@ -0,0 +1,48 @@
+//
+//  Cat.swift
+//  SwiftUINavDemo
+//
+//  Created by Fernando Putallaz on 22/11/2023.
+//
+
+import Foundation
+
+struct Cat: Identifiable, Hashable {
+  var id = UUID()
+  var name: String
+}
+
+struct CatsViewModel {
+  var cats = [
+    Cat(name: "Whiskers"),
+    Cat(name: "Mittens"),
+    Cat(name: "Shadow"),
+    Cat(name: "Luna"),
+    Cat(name: "Oliver"),
+    Cat(name: "Cleo"),
+    Cat(name: "Simba"),
+    Cat(name: "Mocha"),
+    Cat(name: "Bella"),
+    Cat(name: "Tiger"),
+    Cat(name: "Misty"),
+    Cat(name: "Leo"),
+    Cat(name: "Salem"),
+    Cat(name: "Nala"),
+    Cat(name: "Oreo"),
+    Cat(name: "Charlie"),
+    Cat(name: "Ziggy"),
+    Cat(name: "Coco"),
+    Cat(name: "Max"),
+    Cat(name: "Lucy"),
+    Cat(name: "Mittens"),
+    Cat(name: "Shadow"),
+    Cat(name: "Luna"),
+    Cat(name: "Oliver"),
+    Cat(name: "Cleo"),
+    Cat(name: "Simba"),
+    Cat(name: "Mocha"),
+    Cat(name: "Bella"),
+    Cat(name: "Tiger"),
+    Cat(name: "Misty")
+    ]
+}
--- SwiftUINavDemo/SwiftUINavDemo/Dog.swift
+++ SwiftUINavDemo/SwiftUINavDemo/Dog.swift
@@ -12,11 +12,6 @@ struct Dog: Identifiable, Hashable {
   var name: String
 }
 
-struct Cat: Identifiable, Hashable {
-  var id = UUID()
-  var name: String
-}
-
 struct DogViewModel {
   var dogs = [
       Dog(name: "Max"),
@@ -50,40 +45,5 @@ struct DogViewModel {
       Dog(name: "Gracie"),
       Dog(name: "Ellie"),
       Dog(name: "Zoey")
-    ]
-}
-
-struct CatsViewModel {
-  var cats = [
-    Cat(name: "Whiskers"),
-    Cat(name: "Mittens"),
-    Cat(name: "Shadow"),
-    Cat(name: "Luna"),
-    Cat(name: "Oliver"),
-    Cat(name: "Cleo"),
-    Cat(name: "Simba"),
-    Cat(name: "Mocha"),
-    Cat(name: "Bella"),
-    Cat(name: "Tiger"),
-    Cat(name: "Misty"),
-    Cat(name: "Leo"),
-    Cat(name: "Salem"),
-    Cat(name: "Nala"),
-    Cat(name: "Oreo"),
-    Cat(name: "Charlie"),
-    Cat(name: "Ziggy"),
-    Cat(name: "Coco"),
-    Cat(name: "Max"),
-    Cat(name: "Lucy"),
-    Cat(name: "Mittens"),
-    Cat(name: "Shadow"),
-    Cat(name: "Luna"),
-    Cat(name: "Oliver"),
-    Cat(name: "Cleo"),
-    Cat(name: "Simba"),
-    Cat(name: "Mocha"),
-    Cat(name: "Bella"),
-    Cat(name: "Tiger"),
-    Cat(name: "Misty")
     ]
 }
